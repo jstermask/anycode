ff8e2d5d9c79675eaf3edfffdc810096656d6f42
José <jose@labulle.in>
2014-01-26 23:18:49 +0100

    added some code example with fxml

--- anycode-template-editor/src/main/java/in/labulle/anycode/editor/ux/fx/TemplateEditorApplication.java
+++ anycode-template-editor/src/main/java/in/labulle/anycode/editor/ux/fx/TemplateEditorApplication.java
@@ -1,11 +1,9 @@
 package in.labulle.anycode.editor.ux.fx;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class TemplateEditorApplication extends Application {
@@ -16,22 +14,11 @@ public class TemplateEditorApplication extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("Hello World!");
-		Button btn = new Button();
-		btn.setText("Say 'Hello World'");
-		btn.setOnAction(new EventHandler<ActionEvent>() {
-
-			@Override
-			public void handle(ActionEvent event) {
-				System.out.println("Hello World!");
-			}
-		});
-
-		StackPane root = new StackPane();
-		root.getChildren().add(btn);
-		primaryStage.setScene(new Scene(root, 300, 250));
-		primaryStage.show();
-
+		Parent root = FXMLLoader.load(getClass().getResource("main_view.fxml"));
+        Scene scene = new Scene(root, 300, 275);
+        primaryStage.setTitle("FXML Welcome");
+        primaryStage.setScene(scene);
+        primaryStage.show();
 	}
 
 }
