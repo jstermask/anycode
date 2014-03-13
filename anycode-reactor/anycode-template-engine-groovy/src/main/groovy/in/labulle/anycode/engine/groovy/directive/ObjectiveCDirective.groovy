139f84e7f53f006f03e0148e032c6f7e3f69084d
José CARRENO <jose@labulle.in>
2014-03-13 21:47:21 +0100

    commit

---
+++ anycode-reactor/anycode-template-engine-groovy/src/main/groovy/in/labulle/anycode/engine/groovy/directive/ObjectiveCDirective.groovy
@@ -0,0 +1,36 @@
+package in.labulle.anycode.engine.groovy.directive
+
+import in.labulle.anycode.uml.IAttribute;
+import in.labulle.anycode.uml.IDataType;
+
+class ObjectiveCDirective {
+	def attribute(IAttribute a) {
+		
+	}
+	
+	def getDataTypeName(IDataType d) {
+		def dtName = d.getName().toLowerCase()
+		if(d.isPrimitive()) {
+			if(dtName.indexOf("string") != -1) {
+				return "NSString *"
+			} else if(dtName.indexOf("integer") != -1) {
+				return "NSInteger *"
+			} else if(dtName.indexOf("float") != -1) {
+				return "CGFloat *"
+			} else if(dtName.indexOf("date") != -1) {
+				return "NSDate *"
+			} else if(dtName.indexOf("double") != -1) {
+				return "double"
+			} 
+		}
+		return d.getFullyQualifiedName(".")
+	}
+	
+	def datatype(IAttribute a, String collectionClassName) {
+		
+	}
+	
+	def datatype(IAttribute a) {
+		return datatype(a, 'NSArray *')
+	}
+}
