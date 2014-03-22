249b6055f13f6e0314e3afdd12a68b9463ffdedf
José <jose@labulle.in>
2014-03-22 16:17:55 +0100

    JCA : Objective-c directive. lacks an "all dependencies method"

--- anycode-reactor/anycode-template-engine-groovy/src/main/groovy/in/labulle/anycode/engine/groovy/directive/ObjectiveCDirective.groovy
+++ anycode-reactor/anycode-template-engine-groovy/src/main/groovy/in/labulle/anycode/engine/groovy/directive/ObjectiveCDirective.groovy
@@ -4,17 +4,32 @@ import in.labulle.anycode.uml.IAttribute
 import in.labulle.anycode.uml.IClassifier
 
 class ObjectiveCDirective {
-	def attribute(IAttribute a) {
-		
+	/**
+	 * Generates attribute's name
+	 * @param a attribute
+	 * @return attribute's name or datatype name if relation attribute has no specific name.
+	 */
+	def  getAttributeName(IAttribute a) {
+		def attName = (a.getName() != null ? a.getName() : a.getDataType().getName()[0].toLowerCase() + a.getDataType().getName().substring(1))
+		if(!a.getCardinality().isSingle()) {
+			if(a.getName() == null) {
+				attName += "s"
+			}
+		}
+		return attName
 	}
 	
-	def getDataTypeName(IClassifier d) {
+	def attribute(IAttribute a) {
+		return """${datatype(a)} ${getAttributeName(a)};"""
+	}
+	
+	def getDataTypeName(IClassifier d, String modifier) {
 		def dtName = d.getName().toLowerCase()
 		if(d.isPrimitive()) {
 			if(dtName.indexOf("string") != -1) {
 				return "NSString *"
-			} else if(dtName.indexOf("integer") != -1) {
-				return "NSInteger *"
+			} else if(dtName.indexOf("int") != -1) {
+				return "NSInteger"
 			} else if(dtName.indexOf("float") != -1) {
 				return "CGFloat *"
 			} else if(dtName.indexOf("date") != -1) {
@@ -22,12 +37,17 @@ class ObjectiveCDirective {
 			} else if(dtName.indexOf("double") != -1) {
 				return "double"
 			} 
+		} else {
+			return d.getName() + " *" 
 		}
-		return d.getFullyQualifiedName(".")
 	}
 	
 	def datatype(IAttribute a, String collectionClassName) {
-		
+		if(a.getDataType().isPrimitive()) {
+			return """${getDataTypeName(a.dataType, null)}"""
+		} else {
+			return """${!a.cardinality.single ? collectionClassName : getDataTypeName(a.dataType, a.modifier)}"""
+		}
 	}
 	
 	def datatype(IAttribute a) {
